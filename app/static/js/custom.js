60afab9833e1f7daab8522255ff6a1df3c78afdb
Sufian Mushtaq <sufian27@users.noreply.github.com>
2020-01-03 15:19:09 -0600

    inital commit

---
+++ app/static/js/custom.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.typed', {
+    strings: ["Web Development.", "Backend Systems.", "Databases."],
+    typeSpeed: 70,
+    smartBackspace: true, 
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+    lookCount: Infinity,
+    showCursor: true,
+    cursorChar: '|'
+});
