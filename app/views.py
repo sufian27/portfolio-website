5375ac78aee375afb66d663066f0db0996db86ae
Sufian Mushtaq <sufian27@users.noreply.github.com>
2020-01-04 00:22:54 -0600

    fixed errors and added particles.js in home

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/particles.json")
+def send_particles_json():
+    return app.send_static_file('particles.json')
